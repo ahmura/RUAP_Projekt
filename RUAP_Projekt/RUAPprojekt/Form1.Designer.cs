672cee28e388fc856920a14a2e90e05a63826a8b
ahmura <antonio.hmura@gmail.com>
2018-06-24 20:01:29 +0200

    Zavrsen RUAP Projekt

---
+++ RUAP_Projekt/RUAPprojekt/Form1.Designer.cs
@@ -0,0 +1,136 @@
+﻿namespace RUAPprojekt
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.date1 = new System.Windows.Forms.TextBox();
+            this.date2 = new System.Windows.Forms.TextBox();
+            this.date3 = new System.Windows.Forms.TextBox();
+            this.doPrediction = new System.Windows.Forms.Button();
+            this.selectedEntries = new System.Windows.Forms.RadioButton();
+            this.selectedExits = new System.Windows.Forms.RadioButton();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
+            this.label1.Location = new System.Drawing.Point(8, 9);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(323, 20);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Unesite broj putnika za 3 dana unazad:";
+            // 
+            // date1
+            // 
+            this.date1.Location = new System.Drawing.Point(12, 45);
+            this.date1.Name = "date1";
+            this.date1.Size = new System.Drawing.Size(167, 20);
+            this.date1.TabIndex = 1;
+            this.date1.KeyPress += new System.Windows.Forms.KeyPressEventHandler(this.date1_KeyPress);
+            // 
+            // date2
+            // 
+            this.date2.Location = new System.Drawing.Point(12, 71);
+            this.date2.Name = "date2";
+            this.date2.Size = new System.Drawing.Size(167, 20);
+            this.date2.TabIndex = 2;
+            this.date2.KeyPress += new System.Windows.Forms.KeyPressEventHandler(this.date2_KeyPress);
+            // 
+            // date3
+            // 
+            this.date3.Location = new System.Drawing.Point(12, 97);
+            this.date3.Name = "date3";
+            this.date3.Size = new System.Drawing.Size(167, 20);
+            this.date3.TabIndex = 3;
+            this.date3.KeyPress += new System.Windows.Forms.KeyPressEventHandler(this.date3_KeyPress);
+            // 
+            // doPrediction
+            // 
+            this.doPrediction.Location = new System.Drawing.Point(18, 142);
+            this.doPrediction.Name = "doPrediction";
+            this.doPrediction.Size = new System.Drawing.Size(115, 25);
+            this.doPrediction.TabIndex = 4;
+            this.doPrediction.Text = "Obavi predikciju";
+            this.doPrediction.UseVisualStyleBackColor = true;
+            this.doPrediction.Click += new System.EventHandler(this.doPrediction_Click);
+            // 
+            // selectedEntries
+            // 
+            this.selectedEntries.AutoSize = true;
+            this.selectedEntries.Location = new System.Drawing.Point(154, 134);
+            this.selectedEntries.Name = "selectedEntries";
+            this.selectedEntries.Size = new System.Drawing.Size(111, 17);
+            this.selectedEntries.TabIndex = 5;
+            this.selectedEntries.TabStop = true;
+            this.selectedEntries.Text = "Predikcija ulazaka";
+            this.selectedEntries.UseVisualStyleBackColor = true;
+            // 
+            // selectedExits
+            // 
+            this.selectedExits.AutoSize = true;
+            this.selectedExits.Location = new System.Drawing.Point(154, 157);
+            this.selectedExits.Name = "selectedExits";
+            this.selectedExits.Size = new System.Drawing.Size(112, 17);
+            this.selectedExits.TabIndex = 6;
+            this.selectedExits.TabStop = true;
+            this.selectedExits.Text = "Predikcija izlazaka";
+            this.selectedExits.UseVisualStyleBackColor = true;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(346, 186);
+            this.Controls.Add(this.selectedExits);
+            this.Controls.Add(this.selectedEntries);
+            this.Controls.Add(this.doPrediction);
+            this.Controls.Add(this.date3);
+            this.Controls.Add(this.date2);
+            this.Controls.Add(this.date1);
+            this.Controls.Add(this.label1);
+            this.Name = "Form1";
+            this.Text = "Broj putnika jedne stanice u Manhattan-u";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox date1;
+        private System.Windows.Forms.TextBox date2;
+        private System.Windows.Forms.TextBox date3;
+        private System.Windows.Forms.Button doPrediction;
+        private System.Windows.Forms.RadioButton selectedEntries;
+        private System.Windows.Forms.RadioButton selectedExits;
+    }
+}
+
---
+++ RUAP_Projekt/RUAPprojekt/Form1.cs
@@ -0,0 +1,147 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.IO;
+using System.Net.Http;
+using System.Net.Http.Formatting;
+using System.Net.Http.Headers;
+
+namespace RUAPprojekt
+{
+
+    public partial class Form1 : Form
+    {
+
+        public class StringTable
+        {
+            public string[] ColumnNames { get; set; }
+            public string[,] Values { get; set; }
+        }
+
+        public static async Task InvokeRequestResponseService(string key, string dat1, string dat2, string dat3)    //dodani parametri za stvaranje inputa i odabiranje api kljuca za ulaz/izlaz
+        {
+            using (var client = new HttpClient())
+            {
+                var scoreRequest = new
+                {
+
+                    Inputs = new Dictionary<string, StringTable>() {
+                        {
+                            "input1",
+                            new StringTable()
+                            {
+                                ColumnNames = new string[] {"Col1", "Col2", "Col3"},
+                                Values = new string[,] {  { dat1, dat2, dat3 }, }
+                            }
+                        },
+                    },
+                    GlobalParameters = new Dictionary<string, string>()
+                    {
+                    }
+                };
+                string apiKey = key; // Replace this with the API key for the web service
+                client.DefaultRequestHeaders.Authorization = new AuthenticationHeaderValue("Bearer", apiKey);
+
+
+                if(key == "MUBPIvheS07W459cz1sYd7S2uLUuEAju8+3yUcBPJTJbtdSHfq64QKhpVskMfkce5EiyPrZDFKvzb4AWGeB7og==")       //u slucaju da je odabran ulazak putnika
+                {
+                    client.BaseAddress = new Uri("https://ussouthcentral.services.azureml.net/workspaces/ca10e95ffa45402cba2785091231bc41/services/3724c966bea44c17981642b147036881/execute?api-version=2.0&details=true");
+                }
+                else if(key == "fFRaDJSz9XDkGmpFZ6Ui/f9qDldgO+eAu04GQdezxnZ9SROxeiBValm9onIPczsafZQi9J4J1D3psfkmvLxxAQ==")  //u slucaju da je odabran ulazak putnika
+                {
+                    client.BaseAddress = new Uri("https://ussouthcentral.services.azureml.net/workspaces/ca10e95ffa45402cba2785091231bc41/services/c04dd72dd12c4301acc88930803b8958/execute?api-version=2.0&details=true");
+                }
+                
+                // WARNING: The 'await' statement below can result in a deadlock if you are calling this code from the UI thread of an ASP.Net application.
+                // One way to address this would be to call ConfigureAwait(false) so that the execution does not attempt to resume on the original context.
+                // For instance, replace code such as:
+                //      result = await DoSomeTask()
+                // with the following:
+                //      result = await DoSomeTask().ConfigureAwait(false)
+
+
+                HttpResponseMessage response = await client.PostAsJsonAsync("", scoreRequest).ConfigureAwait(false);   //dodan .ConfigureAwait(false) jer se koristi C# aplikacija
+
+                if (response.IsSuccessStatusCode)
+                {
+                    string result = await response.Content.ReadAsStringAsync();
+                    System.IO.File.WriteAllText(@"C:\Users\Antonio\Documents\Visual Studio 2015\Projects\RUAPprojekt\rezultat.json", result); //ispis rezultata predikcije u .json datoteku
+                    MessageBox.Show("Predviđanje uspješno!");               //prikaz uspješno odrađene predikcije u message box-u
+                }
+                else                //prikaz errora u message box-ovima ako do njih dođe
+                {
+                    //Console.WriteLine(string.Format("The request failed with status code: {0}", response.StatusCode));
+                    MessageBox.Show("The request failed with status code: {0}");
+
+                    // Print the headers - they include the requert ID and the timestamp, which are useful for debugging the failure
+                    MessageBox.Show(response.Headers.ToString());
+
+                    string responseContent = await response.Content.ReadAsStringAsync();
+                    MessageBox.Show(responseContent);
+                }
+            }
+        }
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+
+        private void doPrediction_Click(object sender, EventArgs e)
+        {
+            string apikey;
+            string datum1, datum2, datum3;
+            datum1 = date1.Text.ToString();
+            datum2 = date2.Text.ToString();
+            datum3 = date3.Text.ToString();
+
+
+            if (selectedEntries.Checked == true) //ako je odabran broj ulazaka
+            {
+                apikey = "MUBPIvheS07W459cz1sYd7S2uLUuEAju8+3yUcBPJTJbtdSHfq64QKhpVskMfkce5EiyPrZDFKvzb4AWGeB7og==";
+                InvokeRequestResponseService(apikey, datum1, datum2, datum3).Wait();
+            }
+            else if(selectedExits.Checked == true) //ako je odabran broj izlazaka
+            {
+                apikey = "fFRaDJSz9XDkGmpFZ6Ui/f9qDldgO+eAu04GQdezxnZ9SROxeiBValm9onIPczsafZQi9J4J1D3psfkmvLxxAQ==";
+                InvokeRequestResponseService(apikey, datum1, datum2, datum3).Wait();
+            }
+            else        //ako nije odabrano niti jedno
+            {
+                MessageBox.Show("Odaberite koju predikciju želite!");
+            }
+        }
+
+        //kontrola da se u text box-eve upisuju samo brojevi
+        private void date1_KeyPress(object sender, KeyPressEventArgs e)
+        {
+            if (!char.IsControl(e.KeyChar) && !char.IsDigit(e.KeyChar))
+            {
+                e.Handled = true;
+            }
+        }
+
+        private void date2_KeyPress(object sender, KeyPressEventArgs e)
+        {
+            if (!char.IsControl(e.KeyChar) && !char.IsDigit(e.KeyChar))
+            {
+                e.Handled = true;
+            }
+        }
+
+        private void date3_KeyPress(object sender, KeyPressEventArgs e)
+        {
+            if (!char.IsControl(e.KeyChar) && !char.IsDigit(e.KeyChar))
+            {
+                e.Handled = true;
+            }
+        }
+    }
+}
